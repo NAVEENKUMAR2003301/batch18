1a4a02a6734ef95b3172df25b2e063daee588e4c
NAVEENKUMAR2003301 <livnaveen@gmail.com>
2025-10-23 13:10:28 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,143 @@
+// variables 
+
+// var   - global scope
+// let   - block scope
+// const - block scope
+
+
+// rules
+
+// var
+
+var a = 10  // declration // intialiazation
+
+a = 12      // reuse // reIntia
+
+var a = 35  // redeclration
+
+console.log(a);
+
+
+// let 
+
+let b = 1  // declration // intialiazation
+
+b = 5      // reuse //reIntialiazation
+
+// let b = 60 // not accept reDec
+
+console.log(b);
+
+
+// const
+
+const c = 50 // dec // int
+
+// c = 70    // not accept // reuse // reInt
+
+// const c = 60 // not accept reDeclration
+
+console.log(c);
+
+
+// printing Statement
+
+// 1. console.log()
+//  let c2 = 5
+
+// console.log(c2);
+
+
+// 2. alert()
+
+// alert("hello team this is our first class")
+
+// 3. confirm()
+
+// let userWish = confirm("do you like frontend?")
+
+// console.log(userWish);
+
+
+
+// 4. prompt()
+
+// let userName = prompt("what is your name ? ")
+
+// console.log(userName);
+
+
+// 5. document.writeLn
+
+// document.writeln("copyright")
+
+
+// console methods
+
+// console.log()
+
+console.log(100);
+
+
+// console.warn()
+
+console.warn(100)
+
+// console.error()
+
+console.error(100)
+
+// console.clear()
+
+console.clear();
+
+
+
+// DataTypes
+
+// primitive datatypes
+
+// string - "123", "abc"
+
+let str = "one"
+
+console.log((str));
+
+
+// number
+
+let num = 123
+
+console.log(typeof(num));
+
+
+// boolean
+
+let boo = false
+
+console.log(typeof(boo));
+
+
+// undefined
+
+let un;
+
+console.log(un);
+
+
+// null
+
+// let nu = prompt("hi")
+
+// console.log(nu);
+
+
+// non primitive datatypes
+
+// array
+
+// object
+
+// function
+
+// date
